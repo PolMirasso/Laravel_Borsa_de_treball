339c8b73ab6d66a195b3f4d884d07bc4087f977f
PolMirasso <pol.mirasso@gmail.com>
2023-02-10 23:10:40 +0100

    filters, and btn

--- resources/views/student/ajax.blade.php
+++ resources/views/student/ajax.blade.php
@@ -9,6 +9,8 @@
 
 
     <!-- Styles -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 
     <link rel="stylesheet" type="text/css" href="//cdn.datatables.net/1.13.2/css/jquery.dataTables.css">
 
@@ -26,13 +28,13 @@
                         <table id="example1" class="display" style="width:100%">
                             <thead>
                                 <tr>
-                                    <th>id</th>
                                     <th>company_type</th>
                                     <th>company_population</th>
                                     <th>offer_type</th>
                                     <th>working_day_type</th>
                                     <th>offer_sector</th>
                                     <th>characteristics</th>
+                                    <th>Accio</th>
                                 </tr>
                             </thead>
                             <tfoot>
@@ -42,44 +44,34 @@
                                             data-column="0">
                                     </td>
                                     <td>
-                                        <input type="text" class="form-control filter-input" placeholder="filtre 1"
+                                        <input type="text" class="form-control filter-input" placeholder="filtre 2"
                                             data-column="1">
                                     </td>
                                     <td>
-                                        <input type="text" class="form-control filter-input" placeholder="filtre 2"
+                                        <input type="text" class="form-control filter-input" placeholder="filtre 3"
                                             data-column="2">
                                     </td>
                                     <td>
-                                        <input type="text" class="form-control filter-input" placeholder="filtre 3"
+                                        <input type="text" class="form-control filter-input" placeholder="filtre 4"
                                             data-column="3">
                                     </td>
                                     <td>
-                                        <input type="text" class="form-control filter-input" placeholder="filtre 4"
+                                        <input type="text" class="form-control filter-input" placeholder="filtre 5"
                                             data-column="4">
                                     </td>
                                     <td>
-                                        <input type="text" class="form-control filter-input" placeholder="filtre 5"
+                                        <input type="text" class="form-control filter-input" placeholder="filtre 6"
                                             data-column="5">
                                     </td>
-                                    <td>
-                                        <input type="text" class="form-control filter-input" placeholder="filtre 6"
-                                            data-column="6">
-                                    </td>
+
 
                                 </tr>
 
                                 <tr>
+
                                     <td>
                                         <select data-column="0" class="form-control filter-select">
-                                            <option value="">tria opcio</option>
-                                            @foreach ($id as $type)
-                                            <option value="{{ $type }}"> {{ $type }}</option>
-                                            @endforeach
-                                        </select>
-                                    </td>
-                                    <td>
-                                        <select data-column="1" class="form-control filter-select">
-                                            <option value="">tria opcio</option>
+                                            <option value="">tria opcio 1</option>
                                             @foreach ($company_type as $type)
                                             <option value="{{ $type }}"> {{ $type }}</option>
                                             @endforeach
@@ -87,8 +79,8 @@
 
                                     </td>
                                     <td>
-                                        <select data-column="2" class="form-control filter-select">
-                                            <option value="">tria opcio</option>
+                                        <select data-column="1" class="form-control filter-select">
+                                            <option value="">tria opcio 2</option>
                                             @foreach ($company_population as $population)
                                             <option value="{{ $population }}"> {{ $population }}
                                             </option>
@@ -97,8 +89,8 @@
 
                                     </td>
                                     <td>
-                                        <select data-column="3" class="form-control filter-select">
-                                            <option value="">tria opcio</option>
+                                        <select data-column="2" class="form-control filter-select">
+                                            <option value="">tria opcio 3</option>
                                             @foreach ($offer_type as $offer)
                                             <option value="{{ $offer }}"> {{ $offer }}</option>
                                             @endforeach
@@ -106,8 +98,8 @@
 
                                     </td>
                                     <td>
-                                        <select data-column="4" class="form-control filter-select">
-                                            <option value="">tria opcio</option>
+                                        <select data-column="3" class="form-control filter-select">
+                                            <option value="">tria opcio 4</option>
                                             @foreach ($working_day_type as $work_day_type)
                                             <option value="{{ $work_day_type }}"> {{ $work_day_type }}</option>
                                             @endforeach
@@ -116,8 +108,8 @@
                                     </td>
 
                                     <td>
-                                        <select data-column="5" class="form-control filter-select">
-                                            <option value="">tria opcio</option>
+                                        <select data-column="4" class="form-control filter-select">
+                                            <option value="">tria opcio 5</option>
                                             @foreach ($offer_sector as $sector)
                                             <option value="{{ $sector }}"> {{ $sector }}</option>
                                             @endforeach
@@ -126,8 +118,8 @@
                                     </td>
 
                                     <td>
-                                        <select data-column="6" class="form-control filter-select">
-                                            <option value="">tria opcio</option>
+                                        <select data-column="5" class="form-control filter-select">
+                                            <option value="">tria opcio 6</option>
                                             @foreach ($characteristics as $characteristic)
                                             <option value="{{ $characteristic }}"> {{ $characteristic }}</option>
                                             @endforeach
@@ -175,14 +167,27 @@
             url: "https://cdn.datatables.net/plug-ins/1.13.2/i18n/ca.json",
         },
         columns: [
-            { data: 'id' },
-            { data: 'company_type' },
-            { data: 'company_population' },
-            { data: 'offer_type' },
-            { data: 'working_day_type' },
-            { data: 'offer_sector' },
-            { data: 'characteristics' },
+
+
+            { "targets": 0, "data": 'company_type' },
+            { "targets": 1, "data": 'company_population' },
+            { "targets": 2, "data": 'offer_type' },
+            { "targets": 3, "data": 'working_day_type' },
+            { "targets": 4, "data": 'offer_sector' },
+            { "targets": 5, "data": 'characteristics' },
+            {
+                "targets": 6,
+                "data": 'id',
+                orderable: false,
+                "render": function (data, type, row, meta) {
+                    return '<a href="{{ url("/student/' + data + '/edit") }}" class="btn btn-warning"> Reservar </a>';
+                }
+            }
+
+
+
         ],
+
     });
 
     $('.filter-input').keyup(function() {
