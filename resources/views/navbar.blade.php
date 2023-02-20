23ce205c05a0d27cb6e849b8a3a1d682ded07e56
Toni López Llaó <tonilopez450@gmail.com>
2023-02-20 18:22:16 +0100

    blank

--- resources/views/navbar.blade.php
+++ resources/views/navbar.blade.php
@@ -210,7 +210,7 @@
                 
                     @endif
                     <!-- Page Heading -->
-                    <h1 class="h3 mb-4 text-gray-800">Blank Page</h1>
+                    <h1 class="h3 mb-4 text-gray-800"></h1>
 
                     @yield('content')
 
